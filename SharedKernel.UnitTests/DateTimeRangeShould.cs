26d210d89dbc4ae7030dcb37e8fcc25733f93d6b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-08 15:36:44 +0300

    Shared Kernel projects created

---
+++ SharedKernel.UnitTests/DateTimeRangeShould.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedKernel.UnitTests
+{
+    [TestFixture]
+    public class DateTimeRangeShould
+    {
+        private DateTime _testStartDate = new DateTime(1, 1, 2014, 9, 0, 0);
+
+        [Test]
+        //[ExpectedException(typeof(ArgumentException))]
+        public void ThrowArgumentExceptionIfStartDateEqualsEndDate()
+        {
+            var result = new DateTimeRange(_testStartDate, _testStartDate);
+        }
+    }
+}
