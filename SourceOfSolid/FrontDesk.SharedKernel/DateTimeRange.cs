be35606ea12d7e0b24d8e3ca9681aea688492782
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-08 15:53:39 +0300

    builtin functions

--- SourceOfSolid/FrontDesk.SharedKernel/DateTimeRange.cs
+++ SourceOfSolid/FrontDesk.SharedKernel/DateTimeRange.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FrontDesk.SharedKernel
 {
-    public class DateTimeRange
+    public class DateTimeRange : ValueObject<DateTimeRange>
     {
         public DateTime Start { get; private set; }
         public DateTime End { get; private set; }
@@ -16,6 +16,46 @@ namespace FrontDesk.SharedKernel
             Guard.ForPrecedesDate(start, end, "start");
             Start = start;
             End = end;
+        }
+
+        public DateTimeRange(DateTime start, TimeSpan duration) : this(start, start.Add(duration))
+        {
+        }
+
+        protected DateTimeRange() { }
+
+        public int DurationInMinutes()
+        {
+            return (End - Start).Minutes;
+        }
+
+        public DateTimeRange NewEnd(DateTime newEnd)
+        {
+            return new DateTimeRange(this.Start, newEnd);
+        }
+        public DateTimeRange NewDuration(TimeSpan newDuration)
+        {
+            return new DateTimeRange(this.Start, newDuration);
+        }
+        public DateTimeRange NewStart(DateTime newStart)
+        {
+            return new DateTimeRange(newStart, this.End);
+        }
+
+        public static DateTimeRange CreateOneDayRange(DateTime day)
+        {
+            return new DateTimeRange(day, day.AddDays(1));
+        }
+
+        public static DateTimeRange CreateOneWeekRange(DateTime startDay)
+        {
+            return new DateTimeRange(startDay, startDay.AddDays(7));
+        }
+
+        public bool Overlaps(DateTimeRange dateTimeRange)
+        {
+            return this.Start < dateTimeRange.End &&
+                this.End > dateTimeRange.Start;
         }
 
     }
