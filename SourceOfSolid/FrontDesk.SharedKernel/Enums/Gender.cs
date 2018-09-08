5bc681c0912a220d9cf59714ba26ff5a6aee121b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-08 16:07:45 +0300

    Shared Kernel finished

---
+++ SourceOfSolid/FrontDesk.SharedKernel/Enums/Gender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk.SharedKernel.Enums
+{
+    public enum Gender
+    {
+        Female = 1,
+        Male = 2
+    }
+
+    public enum TrackingState
+    {
+        Unchanged = 0,
+        Added = 1,
+        Modified = 2,
+        Deleted = 3
+    }
+}
---
+++ SourceOfSolid/FrontDesk.SharedKernel/Interfaces/IApplicationEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontDesk.SharedKernel.Interfaces
+{
+    public interface IApplicationEvent : IDomainEvent
+    {
+        string EventType { get; }
+    }
+}
