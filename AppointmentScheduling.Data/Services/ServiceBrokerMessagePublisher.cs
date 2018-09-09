4ad874e7ed510c2378fbe58523f3d6fb80e2a160
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-09 13:14:49 +0300

    ServiceBroker publisher

---
+++ AppointmentScheduling.Data/Services/ServiceBrokerMessagePublisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Data.Services
+{
+    public class ServiceBrokerMessagePublisher : IMessagePublisher
+    {
+        private readonly string ConnectionString = "Data Source=localhost;Initial Catalog=ServiceBrokerTest;Integrated Security=True;MultipleActiveResultSets=True";
+        private readonly string MessageType = "SBMessage";
+        private readonly string Contract = "SBContract";
+        private readonly string SchedulerService = "SchedulerService";
+        private readonly string NotifierService = "NotifierService";
+
+        public void Publish(IApplicationEvent applicationEvent)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+                using (var sqlTransaction = sqlConnection.BeginTransaction())
+                {
+                    var conversationHandle = ServiceBrokerWrapper.BeginConversation(sqlTransaction, SchedulerService, NotifierService, Contract);
+
+                    string json = JsonConvert.SerializeObject(applicationEvent, Formatting.None);
+
+                    ServiceBrokerWrapper.Send(sqlTransaction, conversationHandle, MessageType, ServiceBrokerWrapper.GetBytes(json));
+
+                    ServiceBrokerWrapper.EndConversation(sqlTransaction, conversationHandle);
+
+                    sqlTransaction.Commit();
+                }
+                sqlConnection.Close();
+            }
+        }
+    }
+}
