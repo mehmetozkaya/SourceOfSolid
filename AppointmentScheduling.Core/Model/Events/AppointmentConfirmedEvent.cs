90d15861f7f199065d391570692b3d142fa720bf
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-12 08:01:02 +0300

    AppointmentConfirmedEvent

---
+++ AppointmentScheduling.Core/Model/Events/AppointmentConfirmedEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Core.Model.Events
+{
+    public class AppointmentConfirmedEvent : IDomainEvent
+    {
+        public AppointmentConfirmedEvent(Appointment appointment) : this()
+        {
+            AppointmentUpdated = appointment;
+        }
+
+        public AppointmentConfirmedEvent()
+        {
+            this.Id = Guid.NewGuid();
+            DateTimeEventOccurred = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime DateTimeEventOccurred { get; private set; }
+
+        public Appointment AppointmentUpdated { get; private set; }
+    }
+}
