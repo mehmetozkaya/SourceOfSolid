3fc18b99d162e81b9e0e9afac05836ca96d29e01
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-12 07:56:29 +0300

    client - patient

---
+++ AppointmentScheduling.Core/Model/ScheduleAggregate/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Core.Model.ScheduleAggregate
+{
+    public class Client : Entity<int>
+    {
+        public string FullName { get; private set; }
+        public IList<Patient> Patients { get; private set; }
+
+        public Client(int id)
+        {
+            this.Id = id;
+            Patients = new List<Patient>();
+        }
+
+        protected Client() //required for EF
+        {
+        }
+    }
+}
---
+++ AppointmentScheduling.Core/Model/ScheduleAggregate/Patient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Core.Model.ScheduleAggregate
+{
+    public class Patient : Entity<int>
+    {
+        public int ClientId { get; private set; }
+        public string Name { get; private set; }
+        public Gender Gender { get; private set; }
+        public AnimalType AnimalType { get; private set; }
+        public int? PreferredDoctorId { get; set; }
+
+        public Patient(int id)
+            : base(id)
+        {
+        }
+
+        private Patient()
+        {
+
+        }
+    }
+}
