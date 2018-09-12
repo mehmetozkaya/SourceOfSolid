dad1a6bbbf08b1a09ed16837acebec431b31ed49
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-12 07:58:26 +0300

    schedule

---
+++ AppointmentScheduling.Core/Model/ScheduleAggregate/Room.cs
@@ -0,0 +1,19 @@
+﻿using FrontDesk.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Core.Model.ScheduleAggregate
+{
+    public class Room : Entity<int>
+    {
+        public virtual string Name { get; set; }
+
+        public Room(int id)
+            : base(id)
+        {
+        }
+    }
+}
