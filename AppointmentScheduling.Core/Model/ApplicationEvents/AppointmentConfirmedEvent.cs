83a7559f51837685c1f82b80cf2d0ef655068ddc
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-13 08:05:39 +0300

    AppointmentConfirmedEvent

---
+++ AppointmentScheduling.Core/Model/ApplicationEvents/AppointmentConfirmedEvent.cs
@@ -0,0 +1,29 @@
+﻿using FrontDesk.SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Core.Model.ApplicationEvents
+{
+    public class AppointmentConfirmedEvent : IApplicationEvent
+    {
+        public AppointmentConfirmedEvent()
+        {
+            this.Id = Guid.NewGuid();
+            DateTimeEventOccurred = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime DateTimeEventOccurred { get; set; }
+        public Guid AppointmentId { get; set; }
+        public string EventType
+        {
+            get
+            {
+                return "AppointmentConfirmedEvent";
+            }
+        }
+    }
+}
