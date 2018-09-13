541f5880f12547ed641253ce140b545bf3217aad
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-13 08:06:24 +0300

    AppointmentScheduledEvent

---
+++ AppointmentScheduling.Core/Model/ApplicationEvents/AppointmentScheduledEvent.cs
@@ -0,0 +1,32 @@
+﻿using FrontDesk.SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Core.Model.ApplicationEvents
+{
+    public class AppointmentScheduledEvent : IApplicationEvent
+    {
+        public AppointmentScheduledEvent(AppointmentDTO appointment) : this()
+        {
+            AppointmentScheduled = appointment;
+        }
+
+        public AppointmentScheduledEvent()
+        {
+            DateTimeEventOccurred = DateTime.Now;
+        }
+
+        public DateTime DateTimeEventOccurred { get; private set; }
+        public AppointmentDTO AppointmentScheduled { get; private set; }
+        public string EventType
+        {
+            get
+            {
+                return "AppointmentScheduledEvent";
+            }
+        }
+    }
+}
