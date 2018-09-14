c7a5ef1f9b6cfbaddfc3571c705834b068fed4e0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-14 09:19:42 +0300

    Doctor entities

---
+++ ClientPatientManagement.Core/Model/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPatientManagement.Core.Model
+{
+    public class Room : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
