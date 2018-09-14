aef80cc785a329bcc5aab586ca20956084ce76fe
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-14 09:18:25 +0300

    Client - Patient

---
+++ ClientPatientManagement.Core/Model/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPatientManagement.Core.Model
+{
+    public class Patient : IEntity
+    {
+        public Patient(Client owner) : this()
+        {
+            ClientId = owner.Id;
+            Owner = owner;
+            PreferredDoctorId = Owner.PreferredDoctorId;
+        }
+
+        public Patient()
+        {
+        }
+
+        public int Id { get; set; }
+        public Client Owner { get; private set; }
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int? PreferredDoctorId { get; set; }
+    }
+}
