1e50e0ad571cf5c0552643377c0c3901738b15f7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-17 21:17:12 +0300

    IAggregateRoot

--- ClientPatientManagement.Data/Repository.cs
+++ ClientPatientManagement.Data/Repository.cs
@@ -22,6 +22,48 @@ namespace ClientPatientManagement.Data
             this._dbSet = context.Set<TEntity>();
         }
 
+        public IEnumerable<TEntity> List()
+        {
+            return _dbSet.ToList();
+        }
 
+        public TEntity GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var entityToDelete = _dbSet.Find(id);
+            _dbSet.Remove(entityToDelete);
+            _context.SaveChanges();
+        }
     }
+
+    // Sample code below
+
+    public interface IAggregateRoot : IEntity { }
+
+    public class Root : IAggregateRoot
+    {
+        public int Id
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+
 }
