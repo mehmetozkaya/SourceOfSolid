57cfa5e2aa9228e376338b3aa60b1c87becbec97
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-17 21:15:01 +0300

    Rep

---
+++ ClientPatientManagement.Data/Repository.cs
@@ -0,0 +1,17 @@
+﻿using ClientPatientManagement.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPatientManagement.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly CrudContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+
+    }
+}
