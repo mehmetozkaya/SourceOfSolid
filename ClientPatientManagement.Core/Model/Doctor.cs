493c4b647f4514324f7f406050b80fce535daa8b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-18 21:37:50 +0300

    ctor

--- ClientPatientManagement.Core/Model/Doctor.cs
+++ ClientPatientManagement.Core/Model/Doctor.cs
@@ -11,5 +11,10 @@ namespace ClientPatientManagement.Core.Model
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public Doctor(int id, string name)
+        {
+
+        }
     }
 }
