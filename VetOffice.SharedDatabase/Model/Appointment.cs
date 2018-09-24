f0c0413aa3f16363e55fbedcf6f56295c9924965
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-24 11:12:15 -0500

    Appointment

---
+++ VetOffice.SharedDatabase/Model/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetOffice.SharedDatabase.Model
+{
+    public class Appointment
+    {
+        public Guid Id { get; set; }
+        public Guid ScheduleId { get; set; }
+        public int ClientId { get; set; }
+        public int PatientId { get; set; }
+        public int RoomId { get; set; }
+        public int? DoctorId { get; set; }
+        public DateTimeRange TimeRange { get; set; }
+        public int AppointmentTypeId { get; set; }
+        public string Title { get; set; }
+        public DateTime? DateTimeConfirmed { get; set; }
+    }
+}
