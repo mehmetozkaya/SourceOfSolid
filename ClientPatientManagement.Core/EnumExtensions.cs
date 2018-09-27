255cb73c77f2f47799ab4061c4e95b4cb1db4c37
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-27 18:44:21 -0500

    EnumExtensions

---
+++ ClientPatientManagement.Core/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPatientManagement.Core
+{
+    public static class EnumExtensions
+    {
+        public static T GetAttribute<T>(this Enum value) where T : Attribute
+        {
+            var type = value.GetType();
+            var memberInfo = type.GetMember(value.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+            return (T)attributes[0];
+        }
+    }
+}
