113105b60a4bc2e6d006c356cb79d5185730bc1c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-09-28 19:16:43 -0500

    ConvertHelper

--- ClientPatientManagement.Core/ConvertHelper.cs
+++ ClientPatientManagement.Core/ConvertHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,88 @@ namespace ClientPatientManagement.Core
 {
     public static class ConvertHelper
     {
+        private static object TryTo(Type type, object value, object defaultValue, string cultureInfo, out bool convertSucceed)
+        {
+            CultureInfo info = string.IsNullOrEmpty(cultureInfo) ? System.Threading.Thread.CurrentThread.CurrentCulture : new CultureInfo(cultureInfo);
+            convertSucceed = true;
+            try
+            {
+                string valueAsString = value.ToString();
+
+                if (type == typeof(Guid))
+                {
+                    return Guid.Parse(valueAsString);
+                }
+                else if (type.IsEnum)
+                {
+                    return Enum.Parse(type, valueAsString);
+                }
+                else if (type == typeof(Uri))
+                {
+                    if (string.IsNullOrEmpty(valueAsString))
+                    {
+                        return null;
+                    }
+                    return new Uri(valueAsString, UriKind.RelativeOrAbsolute);
+                }
+                else if (value.GetType() == typeof(Guid))
+                {
+                    return ((Guid)value).ToString();
+                }
+                //if (!CheckIsConvertible(value))
+                //{
+                //    return valueAsString;
+                //}
+                return Convert.ChangeType(value, type, info);
+            }
+            catch
+            {
+                convertSucceed = false;
+                return Default(type, value, defaultValue);
+            }
+        }
+
+        public static object Default(Type type, object value, object defaultValue = null)
+        {
+            if (type.IsValueType)
+            {
+                if (defaultValue == null)
+                {
+                    return Activator.CreateInstance(type);
+                }
+                return defaultValue;
+            }
+
+            if (value == null || value == DBNull.Value) return defaultValue;
+
+            if (type == typeof(string))
+            {
+                if (string.IsNullOrEmpty(value.ToString()))
+                    return defaultValue;
+            }
+            return value;
+        }
+
+        public static T Default<T>(T val, T def)
+        {
+            return (T)Default(typeof(T), val, def);
+        }
+
+        public static T To<T>(object val, T def = default(T), string cultureInfo = null)
+        {
+            object result = To(typeof(T), val, def, cultureInfo);
+
+            return (T)result;
+        }
+
+        public static decimal ToDecimal(string value)
+        {
+            var culture = new CultureInfo("tr-TR");
+            var seperator = ",";
+            var clearText = value.Replace(".", seperator).Trim();
+            return decimal.Parse(clearText, CultureInfo.GetCultureInfo("tr-TR"));
+        }
+
 
     }
 }
