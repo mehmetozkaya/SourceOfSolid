7d33105341179c8ad3de6b7ffd7bbf40b5ba8c69
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-02 18:35:19 -0500

    GetEnumByLabel E

--- ClientPatientManagement.Core/EnumLabelConverter.cs
+++ ClientPatientManagement.Core/EnumLabelConverter.cs
@@ -18,5 +18,25 @@ namespace ClientPatientManagement.Core
             }
             return (labelField.GetValue(null)) as string;
         }
+
+        public E GetEnumByLabel<E, L>(string label)
+        {
+            foreach (var item in typeof(L).GetFields())
+            {
+                var value = (string)item.GetValue(null);
+                if (value == label)
+                {
+                    var names = Enum.GetNames(typeof(E));
+                    foreach (var enumName in names)
+                    {
+                        if (enumName == item.Name)
+                        {
+                            return (E)typeof(E).GetField(enumName).GetRawConstantValue();
+                        }
+                    }
+                }
+            }
+            throw new InvalidOperationException("Could not find the value of enum");
+        }
     }
 }
