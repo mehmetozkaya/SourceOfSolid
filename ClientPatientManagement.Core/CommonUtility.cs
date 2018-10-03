7209d682b7807ad2d9cab28f04306e2f70d41295
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-03 17:28:07 -0500

    CommonUtility

---
+++ ClientPatientManagement.Core/CommonUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPatientManagement.Core
+{
+    public class CommonUtility
+    {
+        public static CommonUtility Instance;
+
+        private CommonUtility()
+        {
+            Instance = new CommonUtility();
+        }
+
+
+    }
+}
