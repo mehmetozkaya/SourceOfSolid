02da127f4db4fc8329f540b7fb7ba1525d08c5db
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-09 11:15:04 +0300

    ctor

--- ClientPatientManagement.Core/Context.cs
+++ ClientPatientManagement.Core/Context.cs
@@ -9,5 +9,9 @@ namespace ClientPatientManagement.Core
     public class Context<T>
     {
         private Dictionary<int, T> _dictionaryContext;
+        public String Name { get; private set; }
+
+
+
     }
 }
