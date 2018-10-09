7f7df782c5594ed76e26226bd15a5e33efc7ede5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-09 11:14:34 +0300

    dictionaryContext

---
+++ ClientPatientManagement.Core/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPatientManagement.Core
+{
+    public class Context<T>
+    {
+        private Dictionary<int, T> _dictionaryContext;
+    }
+}
