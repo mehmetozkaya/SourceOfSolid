2721d5614401102f6b8191f5e340c1054abacab5
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-29 20:07:17 +0300

    Factory

--- ClientPatientManagement.Data/Class1.cs
+++ ClientPatientManagement.Data/Class1.cs
@@ -26,5 +26,26 @@ namespace ClientPatientManagement.Data
         }
     }
 
+    public static class Factory
+    {
+        /// <summary>
+        /// Decides which class to instantiate.
+        /// </summary>
+        public static Position Get(int id)
+        {
+            switch (id)
+            {
+                case 0:
+                    return new Manager();
+                case 1:
+                case 2:
+                    return new Clerk();
+                case 3:
+                default:
+                    return new Programmer();
+            }
+        }
+    }
+
 
 }
